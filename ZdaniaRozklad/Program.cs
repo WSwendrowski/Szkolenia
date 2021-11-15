0721497ee7444be3d78571e950c89db1d67839cc
Wojciech Swendrowski <wswendrowski@gmail.com>
2021-11-15 22:40:16 +0100

    add parameter to function

--- ZdaniaRozklad/Program.cs
+++ ZdaniaRozklad/Program.cs
@@ -18,48 +18,15 @@ int itemsInArray = 0;
 int result = 0;
 int valueOfWord = 0;
 
-
-//foreach (string word in arraySentence)
-//{
-//    words += 1;
-//}
-
 words = arraySentence.Length;
 
-//id1 - odmiana słowa "wyraz"
+string wordForms1 = "";
 
-string wordForms1;
-if (words == 1)
-{
-    wordForms1 = "wyraz";
-}
-
-else if ((words >= 2) && (words <= 4))
-{
-    wordForms1 = "wyrazy";
-}
-else
-{
-    wordForms1 = "wyrazów";
-}
-
-//id2 - odmiana słowa "znak"
+wordForms1 = WordVariety(wordForms1);
 
 string wordForms2;
-if ((lenghtSentence >= 0) && (lenghtSentence == 1))
-{
-    wordForms2 = "znak";
-}
 
-else if ((lenghtSentence >= 2) && (lenghtSentence <= 4))
-{
-    wordForms2 = "znaki";
-}
-else
-{
-    wordForms2 = "znaków";
-}
-
+WordVariety2();
 
 Console.WriteLine($"\nTwoje zdanie ma {words} {wordForms1} (i {lenghtSentence} {wordForms2}).\n");
 
@@ -90,5 +57,39 @@ if (valueOfWord >= 1)
     sentence = sentence.Replace("  ", " ");
     sentence = sentence.Replace("!", "");
     sentence = sentence.Replace("?", "");
+}
 
+string WordVariety(string wordForms1)
+{
+    if (words == 1)
+    {
+        wordForms1 = "wyraz";
+    }
+
+    else if ((words >= 2) && (words <= 4))
+    {
+        wordForms1 = "wyrazy";
+    }
+    else
+    {
+        wordForms1 = "wyrazów";
+    }
+    return wordForms1;
+}
+
+void WordVariety2()
+{
+    if ((lenghtSentence >= 0) && (lenghtSentence == 1))
+    {
+        wordForms2 = "znak";
+    }
+
+    else if ((lenghtSentence >= 2) && (lenghtSentence <= 4))
+    {
+        wordForms2 = "znaki";
+    }
+    else
+    {
+        wordForms2 = "znaków";
+    }
 }
