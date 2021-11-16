b64366ad7cf6bfbdb3106ee10d71bade1aae0eb1
Wojciech Swendrowski <wswendrowski@gmail.com>
2021-11-16 20:35:58 +0100

    Add New Projects

---
+++ minutnik/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace minutnik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min = 0;
+            while (min < 60)
+            {
+                int sek = 0;
+                while (sek < 60)
+                {
+                    
+                    Console.WriteLine($"Minut: {min}, sekund: {sek}");
+                    sek++;
+                }
+                min++;
+            }
+        }
+    }
+}
