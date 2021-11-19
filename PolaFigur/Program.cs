f97c5662a455eba35aa237f15b6b1bfcfa0162fe
Rafal Michalowski <rafalek.michalowski@gmail.com>
2021-11-19 22:04:44 +0100

    Works!

--- PolaFigur/Program.cs
+++ PolaFigur/Program.cs
@@ -1,13 +1,15 @@
 ﻿Console.WriteLine("Program do obliczania pól figur dla lamusów by Edek L.");
 string figure;
+bool repeatPlease = true;
 do
-{ 
-Console.WriteLine("\nDostępne figury to:\n\t1. Kwadrat\n\t2. Prostokąt\n\t3. Trójką\n\t4. Koło\n\t5. Koniec programu.");
-Console.WriteLine("\nProszę wpisać numer figury:");
-figure = Console.ReadLine();
-switch (figure)
 {
-    case "1":
+    Console.WriteLine(
+        "\nDostępne figury to:\n\t1. Kwadrat\n\t2. Prostokąt\n\t3. Trójką\n\t4. Koło\n\t5. Koniec programu.");
+    Console.WriteLine("\nProszę wpisać numer figury:");
+    figure = Console.ReadLine();
+    switch (figure)
+    {
+        case "1":
             Console.WriteLine("\nWybraną figurą jest kwadrat.");
             Console.WriteLine("\n\t Proszę podać długość boku kwadratu [cm]:");
             string sideSquare = Console.ReadLine();
@@ -15,8 +17,8 @@ switch (figure)
             Console.WriteLine($"Pole kwadratu wynosi {fieldSquare} cm2.");
             break;
 
-    case "2":
-    
+        case "2":
+
             Console.WriteLine("\nWybraną figurą jest prostokąt.");
             Console.WriteLine("\n\t Proszę podać długość prostokąta [cm]:");
             string widthRactangle = Console.ReadLine();
@@ -24,20 +26,20 @@ switch (figure)
             string highRactangle = Console.ReadLine();
             decimal fieldRactangle = decimal.Parse(widthRactangle) * decimal.Parse(highRactangle);
             Console.WriteLine($"Pole prostokąta wynosi {fieldRactangle} cm2.");
-        break;
-      
-    case "3":
-        
+            break;
+
+        case "3":
+
             Console.WriteLine("\nWybraną figurą jest trójkąt.");
             Console.WriteLine("\n\t Proszę podać szerokość podstawy trójkąta [cm]:");
             string baseTriangle = Console.ReadLine();
             Console.WriteLine("\n\t Proszę podać wysokość trójkąta [cm]:");
             string highTriangle = Console.ReadLine();
-            decimal fieldTriangle = (decimal.Parse(baseTriangle) * decimal.Parse(highTriangle))/2;
+            decimal fieldTriangle = (decimal.Parse(baseTriangle) * decimal.Parse(highTriangle)) / 2;
             Console.WriteLine($"Pole prostokąta wynosi {fieldTriangle} cm2.");
-        break;
+            break;
 
-    case "4":
+        case "4":
 
             Console.WriteLine("\nWybraną figurą jest koło.");
             Console.WriteLine("\n\t Proszę podać promień koła [cm]:");
@@ -45,5 +47,11 @@ switch (figure)
             decimal fieldCircle = (decimal.Parse(radiusCircle) * decimal.Parse(radiusCircle)) * 3.14m;
             Console.WriteLine($"Pole prostokąta wynosi {fieldCircle} cm2.");
             break;
-}
-    while figure;
+
+
+        case "5":
+            repeatPlease = false;
+            break;
+    }
+
+} while (repeatPlease);
