58bc4e03e897579044b25f73ffc7112f7aaf037d
Wojciech Swendrowski <wswendrowski@gmail.com>
2021-12-01 20:30:59 +0100

    poczatek tworzenia dokumentu

--- Faktury/Program.cs
+++ Faktury/Program.cs
@@ -1,25 +1,31 @@
 ﻿Console.WriteLine(@"Witaj w programie do  sprzedaży pyrów i cebuli by Eduardo ""StojącaPała"" Leszczo.");
-const decimal cenaCebuli = 3.99m;
-const decimal cenaPyrow = 1.68m;
+const decimal priceOnion = 3.99m;
+const decimal pricePotatos = 1.68m;
 
 Console.WriteLine("");
 Console.WriteLine("\tChcesz kupić cebulę (1) czy pyry (2)?");
 Console.WriteLine("");
-Console.WriteLine($"Cena netto cebuli:\t {cenaCebuli} PLN");
-Console.WriteLine($"Cena netto pyrów:\t {cenaPyrow} PLN\n");
+Console.WriteLine($"Cena netto cebuli:\t {priceOnion} PLN");
+Console.WriteLine($"Cena netto pyrów:\t {pricePotatos} PLN\n");
 Console.WriteLine("Przyszłem po:");
 
-string wyborAsortymentuKlienta;
-wyborAsortymentuKlienta = Console.ReadLine();
+string clientChooseofItem;
+clientChooseofItem = Console.ReadLine();
 
 Console.WriteLine("\nA ile kg:");
 
-string wyborIlosciKlineta;
-wyborIlosciKlineta = Console.ReadLine();
+string clientChooseOfQty;
+clientChooseOfQty = Console.ReadLine();
 
  void Faktura()
 {
-    Console.WriteLine()
+    string document = "FAKTURA";
+    string placeOf = "Miejsce wystawienia";
+    string miejscowosc = "Gniezno";
+    string headingDate = "Data wystawienia";
+
+
+    Console.WriteLine("Miejsce wystawienia")
 }
 
 
